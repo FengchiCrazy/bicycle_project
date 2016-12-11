b93b337ffedb12945ccc511b9bb54edc1512935a
Fengchi <332016722@qq.com>
2016-12-11 20:48:20 +0800

    Update Crawler

--- step6/weather_crawler.py
+++ step6/weather_crawler.py
@@ -6,37 +6,50 @@ import datetime
 START_DATE = "20100101"
 END_DATE   = "20161130" 
 
-start_date_dt = datetime.datetime.strptime(START_DATE, "%Y%m%d")
-end_date_dt   = datetime.datetime.strptime(END_DATE, "%Y%m%d")
-time_delta    = datetime.timedelta(days=1)
+def get_date_range():
+    start_date_dt = datetime.datetime.strptime(START_DATE, "%Y%m%d")
+    end_date_dt   = datetime.datetime.strptime(END_DATE, "%Y%m%d")
+    time_delta    = datetime.timedelta(days=1)
 
-date_range = []
-tmp_dt = start_date_dt
-while tmp_dt <= end_date_dt:
-    date_range.append(tmp_dt)
-    tmp_dt += time_delta
+    date_range = []
+    tmp_dt = start_date_dt
+    while tmp_dt <= end_date_dt:
+        date_range.append(tmp_dt)
+        tmp_dt += time_delta
 
+    return date_range
 
-with open("weather_data.csv", 'w') as weather:
-    for date in date_range[:1]:
+def get_url(city, datef):
+    url = "https://www.wunderground.com/history/airport/"
+    if city == 'NY':
+        url += 'KNYC/' + datef + "/DailyHistory.html?req_city=New+York&req_state=NY&format=1"
+    elif city == "Chicago":
+        url += "KMDW/" + datef + "/DailyHistory.html?req_city=Chicago&req_statename=Illinois&format=1"
+    elif city == "DC":
+        url += "KDCA/" + datef + "/DailyHistory.html?req_city=Washington&req_state=DC&req_statename=District+of+Columbia&format=1"
     
+    return url
+        
 
-        URL = "https://www.wunderground.com/history/airport/KNYC/" + \
-                date.strftime("%Y/%m/%d") + \
-                "/DailyHistory.html?req_city=New+York&req_state=NY&req_statename=&reqdb.zip=10001&reqdb.magic=10&reqdb.wmo=99999&format=1"
+def crawler(city):
+    with open("weather_data_%s.csv" % city, 'w') as weather:
+        date_range = get_date_range()
+        for date in date_range[:1]:
+            datef = date.strftime("%Y/%m/%d")
+            url = get_url(city, datef)
 
-        content = urllib.request.urlopen(URL).read()
-        content = content.decode()
-        contents = content.strip().split('\n')
-        date_cont = date.strftime("%Y-%m-%d,")
-        for cont in contents[1:]:
-            cont = cont.replace('<br />', '')
-            weather.write(date_cont + cont + '\n')
-            
-        #weather.write(content + '\n')
+            content = urllib.request.urlopen(URL).read()
+            content = content.decode()
+            contents = content.strip().split('\n')
+            date_cont = date.strftime("%Y-%m-%d,")
+            for cont in contents[1:]:
+                cont = cont.replace('<br />', '')
+                weather.write(date_cont + cont + '\n')
 
-        print(date)
+            print(date)
 
+if __name__ == '__main__':
+    crawler("DC")
 
 
 
