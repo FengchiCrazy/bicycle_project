64fae13634c7786251741fe52d16a1c7c5143008
FengchiCrazy <332016722@qq.com>
2017-01-03 23:51:28 +0800

    new york strat

---
+++ new_york/clean_describe.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# *************************************************************
+#       Filename @  clean_describe.py
+#         Author @  Fengchi
+#    Create date @  2017-01-03 23:51:09
+#  Last Modified @  2017-01-03 23:51:11
+#    Description @  
+# *************************************************************
+
+
