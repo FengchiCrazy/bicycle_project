873eecf67a667cb9a5a9c6c62c51f63f8fea4bc5
FengchiCrazy <332016722@qq.com>
2017-01-13 21:25:42 +0800

    update clean for pred

--- new_york/code/clean_for_pred.py
+++ new_york/code/clean_for_pred.py
@@ -5,19 +5,24 @@
 #       Filename @  clean_for_traffic.py
 #         Author @  Fengchi
 #    Create date @  2017-01-09 22:42:49
-#  Last Modified @  2017-01-11 21:51:23
+#  Last Modified @  2017-01-13 20:37:02
 #    Description @  
 # *************************************************************
 
+from collections import OrderedDict
 import datetime
 import os
 import pdb
 
 DATA_PATH    = '/home/dongfengchi/bicycle_data/NYC/data'
 WEATHER_DATA = '/home/dongfengchi/bicycle_data/NYC/weather_data_NYC.csv'
+STATION_DATA = 'stations_count.csv'
+
 TIME_FORMAT1 = "%Y-%m-%d %H:%M:%S"
 TIME_FORMAT2 = "%m/%d/%Y %H:%M"
 TIME_FORMAT3 = "%m/%d/%Y %H:%M:%S"
+
+NA = "NA"
 
 
 def remove_quote(string):
@@ -27,31 +32,19 @@ def remove_quote(string):
         return string[1:-1]
     return string
 
-def get_data_collection():
-    from collections import OrderedDict
+def get_station_count(station_data = STATION_DATA):
+    ret = {}
+    with open(station_data) as data:
+        for line_ in data:
+            line = line_.strip().split(',')
+            year = int(line[0].split('-')[0])
+            month = int(line[0].split('-')[1])
+            count = int(line[1])
 
-    data_dict = OrderedDict()
-
-    years     = range(2013, 2017)
-    months    = range(1, 13)
-    subsciber = ['0', '1']
-    ages      = list(range(10, 71, 10)) + ['>70', 'NA']
-    genders   = ['M', 'F', 'NA']
-
-    # init data_dict
-    for year in years:
-        for month in months:
-            for sub in subsciber:
-                for age in ages:
-                    for gender in genders:
-                        key = tuple([str(x) for x in [year, month, sub, age, gender]])
-                        data_dict[key] = 0
-    return data_dict
-
+            ret[(year, month)] = count
+    return ret
+            
 def get_weather_data(weather_data = WEATHER_DATA, start_date = '2013-07-01', end_date = '2016-09-30'):
-    from collections import OrderedDict
-
-
     WEATHER_DATE_FORMAT = "%Y-%m-%d"
     start_date_dt = datetime.datetime.strptime(start_date, WEATHER_DATE_FORMAT)
     end_date_dt   = datetime.datetime.strptime(end_date, WEATHER_DATE_FORMAT)
@@ -127,11 +120,20 @@ def get_weather_data(weather_data = WEATHER_DATA, start_date = '2013-07-01', end
 if __name__ == "__main__":
     final_data, data_list = get_weather_data()
     print('weather data finished!')
+    station_count_dict = get_station_count()
     #print(weather_data)
     
     #data_dict = get_data_collection()
 
-    for file_name in os.listdir(DATA_PATH):
+    seven_days_list = []
+    add_items = [
+        'station_count',
+        'last_seven_days_count',
+        'count',
+    ]
+    count_day = 0
+    last_date = None
+    for file_name in sorted(os.listdir(DATA_PATH)):
         with open(DATA_PATH + os.sep + file_name) as data:
             header_ = data.readline()
             header = header_.strip().split(',')
@@ -156,21 +158,38 @@ if __name__ == "__main__":
                 day   = rent_time_dt.day
 
                 date_ = datetime.datetime(year, month, day)
+                if date_ != last_date:
+                    seven_days_list.append(count_day)
+                    if len(seven_days_list) >= 7:
+                        last_seven_days_count = seven_days_list.pop(0)
+                    else:
+                        last_seven_days_count = NA
+                    try:
+                        final_data[last_date][add_items[1]] = last_seven_days_count
+                    except:
+                        pass
 
+                    last_date = date_
+                    count_day = 1
+                else:
+                    count_day += 1
+                    
+
+                final_data[date_][add_items[0]] = station_count_dict[(year, month)]
                 if 'count' not in final_data[date_]:
-                    final_data[date_]['count'] = 0
-                final_data[date_]['count'] += 1
+                    final_data[date_][add_items[2]] = 0
+                final_data[date_][add_items[2]] += 1
                 
 
             print('%s finished!' % file_name)
 
     fw = open("pred.csv", "w")
-    data_list.append('count')
+    data_list += add_items
     fw.write(','.join(['year', 'month', 'day', 'weekday'] + data_list) + '\n')
     for key, dict_ in final_data.items():
         to_write = [key.year, key.month, key.day, key.isoweekday()]
         for item in data_list:
-            to_write.append(dict_.get(item, 'NA'))
+            to_write.append(dict_.get(item, NA))
         fw.write(','.join([str(x) for x in to_write]) + '\n')
     fw.close()
 
