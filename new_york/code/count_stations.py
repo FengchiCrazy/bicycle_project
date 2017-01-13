185d888c026baf45534e4d44afcccfb4a801d668
FengchiCrazy <332016722@qq.com>
2017-01-13 21:01:58 +0800

    count station

--- new_york/code/count_stations.py
+++ new_york/code/count_stations.py
@@ -5,27 +5,33 @@
 #       Filename @  count_stations.py
 #         Author @  Fengchi
 #    Create date @  2017-01-12 10:19:05
-#  Last Modified @  2017-01-12 10:29:56
+#  Last Modified @  2017-01-13 20:58:48
 #    Description @  
 # *************************************************************
 
 import os
 
-fw = open('stations_count', 'w')
-for file_name in sorted(os.listdir('data/')):
-    #print(file_name)
-    stations_set = set()
-    with open('data/' + file_name) as data:
-        header = data.readline()
-        
-        for line_ in data:
-            line = line_.strip().split(',')
-            stations_set.add(line[3])
-        
-        if file_name[4] == '-':
-            fw.write(file_name[:7] + ',' +  str(len(stations_set)) + '\n')
-        else:
-            fw.write(file_name[:4] + '-' + file_name[4:6] + ',' + str(len(stations_set)) + '\n')
-        print(file_name + ' finished!')
+DATA_DIR = '/home/dongfengchi/bicycle_data/NYC/data'
+
+def get_month_station(write_file = 'station_count.csv'):
+    fw = open(write_file, 'w')
+    for file_name in sorted(os.listdir(DATA_DIR)):
+        #print(file_name)
+        stations_set = set()
+        with open(DATA_DIR + os.sep + file_name) as data:
+            header = data.readline()
+            
+            for line_ in data:
+                line = line_.strip().split(',')
+                stations_set.add(line[3])
+            
+            if file_name[4] == '-':
+                fw.write(file_name[:7])
+            else:
+                fw.write(file_name[:4] + '-' + file_name[4:6])
+            fw.write(',' + str(len(stations_set))  + ',' + '|'.join([str(x) for x in list(stations_set)])+ '\n')
+            print(file_name + ' finished!')
         
     
+if __name__ == "__main__":
+    get_month_station()    
